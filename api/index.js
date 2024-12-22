e319cc691a4c7a1ebebfb580d9b167d5defbae19
Sanni Opeyemi <sanniopeyemiolayinka@gmail.com>
2024-12-22 20:11:07 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes); 
 app.use("/api/questions", qRoutes); 
 app.use("/api/games", gRoutes); 
-app.use("/api/games", dRoutes); 
+app.use("/api/user", dRoutes); 
 app.use("/api/profile", pRoutes); 
 app.use('/uploads', express.static('uploads'));
 
